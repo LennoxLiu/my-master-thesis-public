6a59dadde02319dbe61c89f2e8a414d5275a0812
Lennox <105095085+LennoxLiu@users.noreply.github.com>
2026-03-15 18:05:12 +0100

    update env

--- hpc/test_gpu.sh
+++ hpc/test_gpu.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=gpu_test
-#SBATCH --output=hpc/gpu_test_%j.out
+#SBATCH --output=hpc/logs/gpu_test_%j.out
 #SBATCH --partition=gpu-single
-#SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:A40:1 
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1 #--gres=gpu:A40:1
 #SBATCH --ntasks=1
 #SBATCH --mem=8G
 #SBATCH --time=00:10:00
 
+mkdir -p hpc/logs
+ 
 # 1. Load the Miniforge and CUDA modules
 module load devel/miniforge/24.9.2
 module load devel/cuda/12.6
-export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 
 # 2. Initialize Conda for this shell session
 source $(conda info --base)/etc/profile.d/conda.sh
