b5ebf65611157977023e69bfcc48b00bdacbdf10
Lennox <105095085+LennoxLiu@users.noreply.github.com>
2026-03-16 19:26:12 +0100

    Update run_reduced_model.sh

--- hpc/run_reduced_model.sh
+++ hpc/run_reduced_model.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=TE_reduced
 #SBATCH --output=hpc/logs/TE_reduced_%a.out
 #SBATCH --partition=gpu-single
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:A40:1
-#SBATCH --ntasks=2
+#SBATCH --array=0-1
 #SBATCH --mem=16G
 #SBATCH --time=00:10:00
 
@@ -24,4 +24,10 @@ conda activate RMDN-TE
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 
 # Run the worker script for this specific array index
-python hpc/hyper_opt_reduced.py --data_file_path "./data/event_times_data.h5" --num_trials 10 --history_length 128 --data_time_length 300 --task_id $SLURM_ARRAY_TASK_ID --seed 42 
+python hpc/hyper_opt_reduced.py \
+    --data_file_path "./data/event_times_data.h5" \
+    --num_trials 10 \
+    --history_length 128 \
+    --data_time_length 300 \
+    --task_id $SLURM_ARRAY_TASK_ID \
+    --seed 42
