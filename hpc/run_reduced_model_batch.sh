2e4676be68fcca798575fa3f525d5f54e3192cc5
Lennox <105095085+LennoxLiu@users.noreply.github.com>
2026-03-17 15:53:37 +0100

    Update run_reduced_model_batch.sh

--- hpc/run_reduced_model_batch.sh
+++ hpc/run_reduced_model_batch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=TE_reduced
 #SBATCH --output=hpc/logs/TE_reduced_arrayjob%a.out
-#SBATCH --partition=devel
+#SBATCH --partition=gpu-single
 #SBATCH --cpus-per-task=16
 #SBATCH --gres=gpu:1
 #SBATCH --array=0
-#SBATCH --mem-per-cpu=4gb
-#SBATCH --time=00:30:00
+#SBATCH --mem-per-cpu=3gb
+#SBATCH --time=02:00:00
 #SBATCH --account=bw20g013
 
 mkdir -p hpc/logs
@@ -37,7 +37,7 @@ for (( i=0; i<$TASKS_PER_JOB; i++ )); do
     
     python hpc/hyper_opt_reduced.py \
         --data_file_path "./data/event_times_data.h5" \
-        --num_trials 5 \
+        --num_trials 50 \
         --history_length 128 \
         --batch_size 512 \
         --data_time_length 900 \
