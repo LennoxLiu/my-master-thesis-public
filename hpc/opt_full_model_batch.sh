ed62f7c58a97df334087417d29f8e9efb81faf68
Elaphurus <105095085+LennoxLiu@users.noreply.github.com>
2026-03-18 17:05:18 +0300

    test 64

--- hpc/opt_full_model_batch.sh
+++ hpc/opt_full_model_batch.sh
@@ -2,11 +2,11 @@
 #SBATCH --job-name=TE_full_opt
 #SBATCH --output=hpc/logs/TE_full_opt_arrayjob%a.out
 #SBATCH --partition=devel
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=32
 #SBATCH --gres=gpu:1
-#SBATCH --array=0-1
+#SBATCH --array=0
 #SBATCH --mem-per-cpu=3gb
-#SBATCH --time=00:30:00
+#SBATCH --time=00:10:00
 #SBATCH --account=bw20g013
 
 mkdir -p hpc/logs
@@ -28,7 +28,7 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 nvidia-cuda-mps-control -d
 
 # Define the number of concurrent tasks per GPU
-TASKS_PER_JOB=32
+TASKS_PER_JOB=64
 #--cpus-per-task should be half of TASKS_PER_JOB
 
 # Launch tasks in the background
