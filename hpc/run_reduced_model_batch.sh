65357c34d5c9e66148aad29c40fa3083b2589f97
Lennox <105095085+LennoxLiu@users.noreply.github.com>
2026-03-20 07:27:47 +0100

    ready run_reduced

--- hpc/run_reduced_model_batch.sh
+++ hpc/run_reduced_model_batch.sh
@@ -2,11 +2,11 @@
 #SBATCH --job-name=TE_reduced_runs
 #SBATCH --output=hpc/logs/TE_reduced_runs_arrayjob%a.out
 #SBATCH --partition=gpu-single
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
-#SBATCH --array=0
-#SBATCH --mem-per-cpu=3gb
-#SBATCH --time=02:00:00
+#SBATCH --array=0-1
+#SBATCH --mem-per-cpu=3328M
+#SBATCH --time=04:00:00
 #SBATCH --account=bw20g013
 
 mkdir -p hpc/logs
@@ -28,7 +28,7 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 nvidia-cuda-mps-control -d
 
 # Define the number of concurrent tasks per GPU
-TASKS_PER_JOB=32
+TASKS_PER_JOB=16
 #--cpus-per-task should be half of TASKS_PER_JOB
 
 # Launch tasks in the background
@@ -37,8 +37,8 @@ for (( i=0; i<$TASKS_PER_JOB; i++ )); do
     
     python hpc/multi_runs_reduced.py \
         --data_file_path "./data/event_times_data.h5" \
-        --num_runs 50 \
-        --history_length 128 \
+        --num_runs 20 \
+        --history_length 512 \
         --data_time_length 900 \
         --task_id $CURRENT_TASK_ID \
         --seed 42 &
